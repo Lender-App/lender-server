b1c51139ab9738b9fe0859c9062a4a01a7d0564e
Alex Chen <alexmchen@gmail.com>
2018-12-13 18:30:01 -0600

    reorganize files, add basic sql handlers

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 import mysql.connector
-
 from flask import Flask
+from datetime import date, datetime, timedelta
+
+
 app = Flask(__name__)
+global cursor
 
-mydb = mysql.connector.connect(
-	host='172.17.0.2',
-	password='test')
-
-print(mydb)
-
-mydb.close()
+insert_request = ('INSERT INTO BorrowRequests '
+                '(BorrowerID, ItemName, Comments, CategoryID, StartTime, EndTime, Price) '
+                'VALUES (%s, %s, %s, %s, %s, %s, %s)')
+select_requests = ('SELECT * FROM BorrowRequests')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/requests/create')
+def create_request():
+    global cursor
+    request_data = (2, 'test', 'test comments', 3,
+        datetime.now().date(), datetime.now().date() + timedelta(days=1), 20)
+    cursor.execute(insert_request, request_data)
+
+    return "Request Created! ID: {}".format(cursor.lastrowid)
+
+@app.route('/requests/')
+def get_requests():
+    global cursor
+    cursor.execute(select_requests)
+    return '\n'.join([str(item) for item in cursor])
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 80)
+    db = mysql.connector.connect(
+        host='172.17.0.2',
+        password='test',
+        database='lender')
+    print(db)
+    cursor = db.cursor()
+
+    app.run(host='0.0.0.0', port=80)
+    db.commit()
+    db.close()
---
+++ mysql/scripts/insertdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO BorrowRequests (BorrowerID, ItemName, CategoryID, StartTime, EndTime, Price)
+VALUES (0,'test item', 0, '2019-12-31 00:00:00', '2019-12-31 23:59:59', 15);
